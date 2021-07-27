1671f64b8d03c3c61e4391bcb0661db35dfa19f0
binaypaul <binay.paul.1993@gmail.com>
2021-07-28 05:20:34 +0530

    Initial project built with springinitializer

---
+++ knote/src/main/java/com/k8s/docker/springboot/knote/KnoteApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.docker.springboot.knote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteApplication.class, args);
+	}
+
+}
