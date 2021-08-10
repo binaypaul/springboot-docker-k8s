73d336c07374fab795e12cf584bd3ec5e32d1959
Binay Paul <binay.paul.1993@gmail.com>
2021-08-10 22:52:28 +0530

    SiplestK8sDockerSpringboot app created

---
+++ SimplestK8sDockerSpringboot/src/test/java/com/binaypaul/SimplestK8sDockerSpringboot/SimplestK8sDockerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaypaul.SimplestK8sDockerSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplestK8sDockerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
