acfbb0787d81e05af63bcd60adde4b5fa43e5380
Binay Paul <binay.paul.1993@gmail.com>
2021-08-10 19:46:41 +0530

    test changes and cheatsheet added

--- knote/src/main/java/com/k8s/docker/springboot/knote/KnoteApplication.java
+++ knote/src/main/java/com/k8s/docker/springboot/knote/KnoteApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KnoteApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
 		SpringApplication.run(KnoteApplication.class, args);
 	}
-
 }
